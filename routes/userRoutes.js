0009d028e6e8bfb0a6429c896efb92250f799f99
CHANDRAKANT GAWANDE <102407610+cgawande@users.noreply.github.com>
2023-03-25 18:09:16 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express=require("express")
 const router=express.Router()
 
-const {upload}=require("../middleware/multerMiddleware")
+const {upload,multerUpload}=require("../middleware/multerMiddleware")
 const userController=require("../controller/userController")
 const {checkUserAuth} =require("../middleware/authMiddleware")
 
-router.post("/signup",upload.single("profilePic"),userController.userSignup)
+router.post("/signup",multerUpload.single("profilePic"),userController.userSignup)
 router.post("/login",userController.userLogin)
 router.post("/logout",checkUserAuth,userController.userLogout)
 
